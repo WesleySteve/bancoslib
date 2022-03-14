4af08f65193265d27a98c95abb72677f160d1aef
Wesley Steve <wesley.silva23@hotmail.com>
2022-03-14 16:00:11 -0300

    retirado o .env e adicionado o path

--- bancoslib/db/sqlite.py
+++ bancoslib/db/sqlite.py
@@ -5,17 +5,16 @@ import sqlalchemy
 
 
 
-def create_db_sqlite(dotenv_path=os.path.expanduser("~/.env")):
+def create_db_sqlite(path):
     
-    if (dotenv_path != "~/.env"):
     
-        # definindo string de conexao com o banco de dados sqlite
-        str_conexao = "sqlite:///{path}"
+    
+    # definindo string de conexao com o banco de dados sqlite
+    str_conexao = "sqlite:///{path}"
         
-        # carregando o dotenv
-   # str_conexao = str_conexao.format(path=os.path.join(DATA_OLTP_DIR, "olist_oltp.db"))
-        str_conexao = str_conexao.format(
-                path=os.path.join(dotenv.load_dotenv(dotenv_path), "olist_oltp.db"))
+    # carregando o dotenv
+    str_conexao = str_conexao.format(path=os.path.join(path, "olist_oltp.db"))
+    
     
     # criando a conexao com o banco de dados sqlite
     con = sqlalchemy.create_engine(str_conexao)

af242ef2367fdfad086bee142bc1fdeca50f853f
Wesley Steve <wesley.silva23@hotmail.com>
2022-07-21 08:57:37 -0300

    feat: connect_db_sqlite adicionado a funcao

--- bancoslib/db/sqlite.py
+++ bancoslib/db/sqlite.py
@@ -3,22 +3,31 @@ import os
 import sqlalchemy
 
 
+def create_db_sqlite(path, nome_banco):
 
-def create_db_sqlite(path):
-    
-    
-    
     # definindo string de conexao com o banco de dados sqlite
     str_conexao = "sqlite:///{path}"
-        
+
     # carregando o dotenv
-    str_conexao = str_conexao.format(path=os.path.join(path, "olist_oltp.db"))
-    
-    
+    str_conexao = str_conexao.format(path=os.path.join(path, nome_banco + ".db"))
+
     # criando a conexao com o banco de dados sqlite
     con = sqlalchemy.create_engine(str_conexao)
 
     return con
 
 
+def connect_db_sqlite(db_path):
+    """Função para se conectar com o banco de dados local sqlite
 
+    Returns:
+        con: retorna uma conexao com o banco de dados sqlite
+    """
+
+    # definindo string de conexao com o banco de dados sqlite
+    str_conexao = "sqlite:///{db_path}".format(db_path)
+
+    # criando a conexao com o banco de dados sqlite
+    con = sqlalchemy.create_engine(str_conexao)
+
+    return con

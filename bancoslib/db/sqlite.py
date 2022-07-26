3e66579caa21b9f5136f9aee7881b4d2cc18e989
Wesley Steve <wesley.silva23@hotmail.com>
2022-07-26 09:54:31 -0300

    refactor: connect_db_sqlite adicionado nome_banco como parametro de entrada da funcao

--- bancoslib/db/sqlite.py
+++ bancoslib/db/sqlite.py
@@ -17,7 +17,7 @@ def create_db_sqlite(path, nome_banco):
     return con
 
 
-def connect_db_sqlite(db_path):
+def connect_db_sqlite(db_path, nome_banco):
     """Função para se conectar com o banco de dados local sqlite
 
     Returns:
@@ -25,7 +25,10 @@ def connect_db_sqlite(db_path):
     """
 
     # definindo string de conexao com o banco de dados sqlite
-    str_conexao = "sqlite:///{db_path}".format(db_path)
+    str_conexao = "sqlite:///{db_path}"
+
+    # definindo string de conexao com o banco de dados sqlite
+    str_conexao = str_conexao.format(path=os.path.join(db_path, nome_banco))
 
     # criando a conexao com o banco de dados sqlite
     con = sqlalchemy.create_engine(str_conexao)
